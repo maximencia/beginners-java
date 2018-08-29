86335d6666c173f6319328cb677157a2917fc1a1
maxim.rumyantsev <maxim.rumyantsev@billing.ru>
2018-08-29 17:45:56 +0300

    lesson 1- 3

---
+++ src/main/java/lesson_2_variables.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Maxim.Rumyantsev on 29-Aug-18.
+ */
+public class lesson_2_variables {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        int myInt = 577;
+        short myShort = 3266;
+        long myLong = 26526523;
+
+        double myDouble = 235.36;
+        float myFloat=2365.4f;
+
+        char c='a';
+        boolean b =true;
+        byte myBite=100; // -128 -127
+
+        System.out.println(myInt);
+    }
+}
