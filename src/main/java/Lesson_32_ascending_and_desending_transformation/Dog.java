dec58185440cc1b334db5e8432c3b073e8dfef16
maxim.rumyantsev <maxim.rumyantsev@billing.ru>
2018-09-19 11:20:04 +0300

    Lesson_32_ascending_and_desending_transformation

---
+++ src/main/java/Lesson_32_ascending_and_desending_transformation/Dog.java
@@ -0,0 +1,10 @@
+package Lesson_32_ascending_and_desending_transformation;
+
+/**
+ * Created by Maxim.Rumyantsev on 19-Sep-18.
+ */
+public class Dog extends Animal {
+   public void Bark(){
+        System.out.println("I am barking");
+    }
+}
